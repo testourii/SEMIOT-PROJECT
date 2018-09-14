671edb71ad344d3a8b4c4ab5795e19b8c0046ba8
testourii <houcemtestouriiii@gmail.com>
2018-09-14 22:38:11 +0200

    Add files via upload

---
+++ SEMIoT Node/SEMIOT.js
@@ -0,0 +1,10 @@
+module.exports = function(RED) {
+    function test(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("SEMIOT",test);
+}
